95afd25f12e5ad4f5ff23c70f67399b985a1e2dd
moguy93 <guymorgan@live.fr>
2023-11-29 15:49:21 +0100

    add correction exercice 1 POO

--- corrections/corrections_poo/exercice1.cpp
+++ corrections/corrections_poo/exercice1.cpp
@@ -1,3 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CompteBancaire
+{
+public:
+    string titulaire;
+    string numeroCompte;
+    double soldeCompte;
+
+    CompteBancaire(const string &titulaire = "", const string &numeroCompte = "", double soldeCompte = 0.0) : titulaire(titulaire), numeroCompte(numeroCompte), soldeCompte(soldeCompte) {}
+
+    void deposer(double montant)
+    {
+        if (montant > 0)
+        {
+            soldeCompte += montant;
+        }
+    }
+
+    bool retirer(double montant)
+    {
+        if (montant > 0 && soldeCompte >= montant)
+        {
+            soldeCompte -= montant;
+            return true;
+        }
+        return false;
+    }
+
+    double obtenirSolde() const
+    {
+        return soldeCompte;
+    }
+
+    void afficherInfos() const
+    {
+        cout << "Titulaire: " << titulaire << endl;
+        cout << "Numero du compte: " << numeroCompte << endl;
+        cout << "Solde: " << soldeCompte << endl;
+    }
+};
+
 int main()
 {
     // Création d'un compte avec le constructeur par défaut
@@ -5,9 +50,10 @@ int main()
     compte1.afficherInfos();
 
     // Création d'un compte avec des informations spécifiques
-    CompteBancaire compte2("Jean Dupont", "123456789");
-    compte2.deposer(1000.0); // Dépôt d'argent
-    compte2.retirer(200.0);  // Retrait d'argent
+    CompteBancaire compte2("Jean Dupont", "123456789", 2000.27);
+    compte2.deposer(800.0);                        // Dépôt d'argent
+    const bool validite = compte2.retirer(3000.0); // Retrait d'argent
+    (validite) ? cout << "Retrait reussi.\n" : cout << "Retrait echoue.\n";
     compte2.afficherInfos(); // Affichage des informations
 
     return 0;
