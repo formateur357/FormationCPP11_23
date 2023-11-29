f15e6051bc7bf2421c1475a1ae1b31200c452dd1
DESKTOP-CSDGFS2\Florent <florent.giaccherini@sii.fr>
2023-11-29 12:02:08 +0100

    add exo

--- rendu/florent/exo2.cpp
+++ rendu/florent/exo2.cpp
@@ -2,30 +2,30 @@
 
 using namespace std;
 
-int NB_NOTE = 3;
+int NB_SCORE = 3;
 
-double getScoreAverage(double scoreList[])
+double getScoreAverage(const double scoreList[])
 {
 
     int i = 0;
-    double scoreSum = 0;
+    double scoreSum = 0.0;
 
-    while (i < NB_NOTE)
+    while (i < NB_SCORE)
     {
         scoreSum += scoreList[i];
         ++i;
     }
 
-    return scoreSum / (double)NB_NOTE;
+    return scoreSum / (double)NB_SCORE;
 }
 
-double getHigherScore(double scoreList[])
+double getHigherScore(const double scoreList[])
 {
 
-    int i = 0;
-    double higherScore = 0;
+    double higherScore = scoreList[0];
+    int i = 1;
 
-    while (i < NB_NOTE)
+    do
     {
         auto score = scoreList[i];
         if (score > higherScore)
@@ -33,19 +33,18 @@ double getHigherScore(double scoreList[])
             higherScore = score;
         }
         ++i;
-    }
+    } while (i < NB_SCORE);
 
     return higherScore;
 }
 
-void showAverageScore(double scoreList[])
+void showAverageScore(const double scoreList[])
 {
-
     auto scoreAverage = getScoreAverage(scoreList);
     cout << "La moyenne est : " << scoreAverage << endl;
 }
 
-void showHigherScore(double scoreList[])
+void showHigherScore(const double scoreList[])
 {
 
     auto higherScore = getHigherScore(scoreList);
@@ -55,25 +54,28 @@ void showHigherScore(double scoreList[])
 void setScoreList(double scoreList[])
 {
 
-    for (int i = 0; i < NB_NOTE; i++)
+    for (int i = 0; i < NB_SCORE; i++)
     {
         cout << "Entrez la note numero " << (i + 1) << ": " << endl;
         cin >> scoreList[i];
     }
 }
+
 int main()
 {
     bool stopLoop = false;
-    double scoreList[NB_NOTE];
+    double scoreList[NB_SCORE];
+    int userReturn = 0;
 
     while (!stopLoop)
     {
         cout << "\nVoulez vous: \n";
-        cout << "\t - 1 = Renseigner " << NB_NOTE << " note" << ((NB_NOTE > 1) ? "s" : "") << endl;
+        cout << "\t - 1 = Renseigner " << NB_SCORE << " note" << ((NB_SCORE > 1) ? "s" : "") << endl;
         cout << "\t - 2 = Voir la moyenne\n";
         cout << "\t - 3 = Voir la note la plus élevée\n";
+        cout << "\t - 4 = Quitter \n";
 
-        int userReturn = 0;
+        cout << "Entrez votre choix : ";
         cin >> userReturn;
 
         if (userReturn == 1)
@@ -88,10 +90,13 @@ int main()
         {
             showHigherScore(scoreList);
         }
+        else if (userReturn == 4)
+        {
+            stopLoop = true;
+        }
         else
         {
             cout << "Je n'ai pas compris votre reponse\n";
-            stopLoop = true;
         }
     }
 
