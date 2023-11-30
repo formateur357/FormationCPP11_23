4251c0ee349ee01d6dce9a41fc43dd3b54c05cbe
Pfefer <117268655+Pfefer@users.noreply.github.com>
2023-11-30 11:23:16 +0100

    Add files via upload
    update exo avec reductibilité

--- rendu/antoine/Rational.cpp
+++ rendu/antoine/Rational.cpp
@@ -2,22 +2,39 @@
 #include <iostream>
 using namespace std;
 
-Rational::Rational(signed int numerateur, int denominateur) : m_numerateur(numerateur), m_denominateur(denominateur)
+Rational::Rational(signed int numerateur, int denominateur) : m_numerateur(numerateur/ pgcd(numerateur, denominateur)), m_denominateur(denominateur / pgcd(numerateur, denominateur))
 {
 	if (m_denominateur == 0) {
 		cout << "fraction non définie" << endl;
 	}
-
+	
 }
 
 Rational::Rational(signed int numerateur) : m_numerateur(numerateur), m_denominateur(1)
 {
+
+}
+
+int Rational::pgcd(int a, int b)
+{
+	while (b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+}
+
+void Rational::reduc(Rational& frac)
+{
+	frac.m_numerateur/= pgcd(frac.m_numerateur, frac.m_denominateur);
+	frac.m_denominateur /= pgcd(frac.m_numerateur, frac.m_denominateur);
+
 }
 
 Rational operator+(Rational const& a, Rational const& b)
 {	
-	
-	return Rational(a.m_numerateur * b.m_denominateur + b.m_numerateur * a.m_denominateur, a.m_denominateur * b.m_denominateur );
+	return Rational(a.m_numerateur * b.m_denominateur + b.m_numerateur * a.m_denominateur, a.m_denominateur * b.m_denominateur);
 }
 
 Rational operator-(Rational const& a, Rational const& b)
